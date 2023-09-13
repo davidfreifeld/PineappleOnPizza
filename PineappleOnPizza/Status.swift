515b24c141ea56916319e4a6f7b53b384c7621a7
David Freifeld <david.freifeld@gmail.com>
2023-09-13 13:49:48 -0600

    added archive status, moved FinalResultsView into folder

--- PineappleOnPizza/Status.swift
+++ PineappleOnPizza/Status.swift
@@ -12,6 +12,7 @@ enum Status: Int, PersistableEnum {
     case new = 1
     case open = 2
     case completed = 3
+    case archived = 4
     var name: String {
         switch self {
         case .new:
@@ -20,13 +21,20 @@ enum Status: Int, PersistableEnum {
             return "Open"
         case .completed:
             return "Completed"
+        case .archived:
+            return "Archived"
         }
     }
     var rowColor: Color {
         switch self {
-        case .new: return Color("NewSurveyColor")
-        case .open: return Color("OpenSurveyColor")
-        case .completed: return Color("CompletedSurveyColor")
+        case .new:
+            return Color("NewSurveyColor")
+        case .open:
+            return Color("OpenSurveyColor")
+        case .completed:
+            return Color("CompletedSurveyColor")
+        case .archived:
+            return Color("CompletedSurveyColor")
         }
     }
 }
